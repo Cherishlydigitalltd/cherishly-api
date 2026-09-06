3385e79ab36c4a4b7fe17863ae28dce7521a1124
OlulekeOd <olulekeodunuga@yahoo.com>
2026-09-06 06:37:34 +0100

    update

--- app/Http/Controllers/Admin/AdminRegistryController.php
+++ app/Http/Controllers/Admin/AdminRegistryController.php
@@ -29,7 +29,18 @@ class AdminRegistryController extends Controller
 
     public function show(GiftRegistry $registry): JsonResponse
     {
-        $registry->load(['user:id,first_name,last_name,email', 'gifts']);
+        $registry->load([
+            'user:id,first_name,last_name,email',
+            'gifts' => function ($q) {
+                $q->with([
+                    'contributions' => function ($q) {
+                        $q->where('payment_status', 'successful')
+                            ->select(['id', 'gift_id', 'donor_name', 'donor_email', 'donor_phone', 'amount', 'is_anonymous', 'payment_reference', 'created_at'])
+                            ->latest();
+                    }
+                ]);
+            }
+        ]);
         return ApiResponse::success('Registry retrieved.', $registry);
     }
 
