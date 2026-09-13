6ac1b7377f28eb76afdc05b1cdd81476e1c36d1c
OlulekeOd <olulekeodunuga@yahoo.com>
2026-09-13 11:24:13 +0100

    update

---
+++ database/seeders/SettingSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SettingSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $settings = [
+            [
+                'key' => 'transaction_fee_rate',
+                'value' => '2.5',
+                'description' => 'Platform transaction fee percentage applied to all contributions',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
+
+        foreach ($settings as $setting) {
+            DB::table('settings')->updateOrInsert(
+                ['key' => $setting['key']],
+                $setting
+            );
+        }
+    }
+}
